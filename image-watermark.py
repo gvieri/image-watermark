05486c3c80e81ace284429d8b3236f7feec0bfd8
Giovambattista Vieri <gvieri@users.noreply.github.com>
2023-01-13 11:20:34 +0100

    Update image-watermark.py
    improvements

--- image-watermark.py
+++ image-watermark.py
@@ -17,15 +17,16 @@ from pprint import pprint
 
 
 def getOptions(args=sys.argv[1:]):
-    parser=argparse.ArgumentParser(description='This simple program will help you to watermark an image')
+    parser=argparse.ArgumentParser(description='This simple program will help you to watermark an image. The -i parameter is the image file name. This name will be added of a ".out." between the file name and the extension. The resulting name will be used for the output file.',epilog='Example of use:')
     parser.add_argument('-i','--imagename', help='image file name', action='store', dest="imagefilename", required=True )
     parser.add_argument('-m','--message', help='message to be watermark (you can use "" to insert more words', action='store',default='',  dest='message' )
     parser.add_argument('-f','--font', help='font', action='store',default='0',  dest='font', choices=['0','1','2','3','4','5','6','7'] )
+    parser.add_argument('-d','--dateformat', help='dateformat 0 for 12 Jun, 2018, 1 for 2018-06-12, 2 for 06/12/2018', action='store',default='0',  dest='dateformat', choices=[0,1,2],type=int )
     parser.add_argument('-a','--alpha', help='alpha (transparency) ', action='store',default='0.3',  dest='alpha', type=float )
+    parser.add_argument('-t','--thickness', help='thickness (of font) ', action='store',default='2',  dest='thickness', type=int )
     parser.add_argument('-v','--verbose', help='more verbose output', action='store_true')
     opt=parser.parse_args(args)
     return(opt)
-
 
 ##########################################
 if __name__ == "__main__":
@@ -47,13 +48,18 @@ if __name__ == "__main__":
         print ("width    : = ",width)
         print ("channels : = ",channels)
     overlay =  np.zeros((height, width, channels), dtype=np.uint8)
+    fdate=""
+    if(opt.dateformat==0):
+        fodate="%d %b, %Y"
+    elif (opt.dateformat==1):
+        fodate="%m-%d-%Y"
+    elif (opt.dateformat==2):
+        fodate="%m/%d/%Y"
 
-
-
-
+    d=(today.strftime(fodate))
 # Stamp the message on image lower right corner 
-    cv2.putText(overlay, str(today), (int(width/10),int (height/8)), int(opt.font), 2.5, (255,255,255), thickness=2)
-    cv2.putText(overlay, opt.message, (int(width/10),int (height/8*7)), cv2.FONT_HERSHEY_PLAIN, 2.5, (255,255,255), thickness=2)
+    cv2.putText(overlay, d, (int(width/10),int (2+height/8)), int(opt.font), 2.5, (255,255,255), thickness=opt.thickness)
+    cv2.putText(overlay, opt.message, (int(width/10),int (height/8*7)), cv2.FONT_HERSHEY_PLAIN, 2.5, (255,255,255), thickness=opt.thickness)
     if(verbose):
         cv2.imshow("overlay",overlay)
     cv2.addWeighted(overlay, opt.alpha, output, 1 - opt.alpha, 0, output)
@@ -63,3 +69,5 @@ if __name__ == "__main__":
     cv2.imwrite(opt.imagefilename+".out.jpg", output)
     if(verbose):
         cv2.waitKey(15000)
+        
+ 
